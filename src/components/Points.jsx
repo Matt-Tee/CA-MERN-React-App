a68812dd1afab7da906da7da5ebd06e4d3826007
Matthew <matthew.leacy@live.com.au>
2019-07-18 15:56:42 +1000

    wrote sort functions, they still need implementing

--- src/components/Points.jsx
+++ src/components/Points.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table } from 'bloomer';
 import bulma from 'bulma';
 import axios from 'axios';
@@ -10,18 +10,40 @@ export default function Points() {
   const [users, setUsers] = useState(null)
 
   useEffect(() => {
-      dataAPI.get('/').then(response => {setUsers(response.data)})
-  },[]);
+    dataAPI.get('/').then(response => { setUsers(response.data) })
+  }, []);
 
-  function loading(){
-    return(
-      <tr>Loading...</tr>
+  function loading() {
+    return (
+      <tr><td colSpan="3">Loading...</td></tr>
     )
   }
-    
-  function renderData(data){
+
+  function sortByPoints() {
+    let sortedUsers = [].concat(users).sort((a, b) => {
+      return parseInt(a.points) - parseInt(b.points);
+    });
+    setUsers(sortedUsers);
+  }
+
+  function sortById() {
+    let sortedUsers = [].concat(users).sort((a, b) => {
+      return parseInt(a.user_id) - parseInt(b.user_id);
+    });
+    setUsers(sortedUsers);
+  }
+
+  function sortByUsername() {
+    let sortedUsers = [].concat(users).sort((a, b) => {
+      return a.username.localeCompare(b.username);
+    });
+    setUsers(sortedUsers);
+  }
+
+  function renderData(data) {
+
     return (data.map((user, index) => {
-      const { user_id, username, points } = user 
+      const { user_id, username, points } = user
       return (
         <tr key={index}>
           <td>{user_id}</td>
@@ -37,13 +59,13 @@ export default function Points() {
       <Table isBordered isStriped>
         <thead>
           <tr>
-            <th>ID</th>
+            <th >ID</th>
             <th>Username</th>
             <th>Points</th>
           </tr>
         </thead>
         <tbody>
-          {!users ? loading():renderData(users)}
+          {!users ? loading() : renderData(users)}
         </tbody>
       </Table>
     </div>
