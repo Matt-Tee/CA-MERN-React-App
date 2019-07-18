0970694893102d095ebae0c72b5fe8674dd187e4
Matthew <matthew.leacy@live.com.au>
2019-07-18 15:03:31 +1000

    Point table makes successful api get request

--- src/components/Points.jsx
+++ src/components/Points.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Table } from 'bloomer';
+import bulma from 'bulma';
+import axios from 'axios';
+const dataAPI = axios.create({ baseURL: 'https://stormy-tundra-35633.herokuapp.com/' });
+
 
 export default function Points() {
+
+  const [users, setUsers] = useState(null)
+
+  useEffect(() => {
+      dataAPI.get('/').then(response => {setUsers(response.data)})
+  },[]);
+
+  function loading(){
+    return(
+      <tr>Loading...</tr>
+    )
+  }
+    
+  function renderData(data){
+    return (data.map((user, index) => {
+      const { user_id, username, points } = user 
+      return (
+        <tr key={index}>
+          <td>{user_id}</td>
+          <td>{username}</td>
+          <td>{points}</td>
+        </tr>
+      )
+    }))
+  }
+
   return (
     <div>
-      <p>Table of users and their points goes here</p>
+      <Table isBordered isStriped>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!users ? loading():renderData(users)}
+        </tbody>
+      </Table>
     </div>
   );
 }
