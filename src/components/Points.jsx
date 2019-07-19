e1bb6fec741dbbec17c1cf4017483fa7f5e4ecef
Matthew <matthew.leacy@live.com.au>
2019-07-19 10:51:05 +1000

    point sorting integrated

--- src/components/Points.jsx
+++ src/components/Points.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Table } from 'bloomer';
+import { Table, Button } from 'bloomer';
 import bulma from 'bulma';
 import axios from 'axios';
 const dataAPI = axios.create({ baseURL: 'https://stormy-tundra-35633.herokuapp.com/' });
@@ -11,6 +11,7 @@ export default function Points() {
 
   useEffect(() => {
     dataAPI.get('/').then(response => { setUsers(response.data) })
+    // setUsers([{user_id: 12335, username: 'bob', points: 5},{user_id: 35790, username: 'sarah', points: 12},{user_id: 734623, username: 'tommy', points: 7},{user_id: 67236, username: 'nubnub', points: 1},{user_id: 235789342, username: 'samuel', points: 3}])
   }, []);
 
   function loading() {
@@ -19,21 +20,27 @@ export default function Points() {
     )
   }
 
-  function sortByPoints() {
+  function sortByPoints(e) {
+    e.preventDefault();
+
     let sortedUsers = [].concat(users).sort((a, b) => {
-      return parseInt(a.points) - parseInt(b.points);
+      return parseInt(b.points) - parseInt(a.points);
     });
+    console.log(sortedUsers)
+
     setUsers(sortedUsers);
   }
 
-  function sortById() {
+  function sortById(e) {
+    e.preventDefault();
     let sortedUsers = [].concat(users).sort((a, b) => {
       return parseInt(a.user_id) - parseInt(b.user_id);
     });
     setUsers(sortedUsers);
   }
 
-  function sortByUsername() {
+  function sortByUsername(e) {
+    e.preventDefault();
     let sortedUsers = [].concat(users).sort((a, b) => {
       return a.username.localeCompare(b.username);
     });
@@ -56,6 +63,9 @@ export default function Points() {
 
   return (
     <div>
+     <Button isColor="primary" onClick={sortById}>ID</Button>
+     <Button isColor="primary" onClick={sortByUsername}>Username</Button>
+     <Button isColor="primary" onClick={sortByPoints}>Points</Button>
       <Table isBordered isStriped>
         <thead>
           <tr>
