d33d7eef557b6fc0400f69b77e2102c8afc8a202
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-07-23 14:16:35 +1000

    private routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, Component} from 'react';
 import Points from './components/Points';
 import Logs from './components/Logs';
-import { Route, BrowserRouter as Router, Link } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Link,  } from 'react-router-dom'
 import GreenbotNavbar from './components/Navbar';
 import TakeParams from './TakeParams'
 import Administrators from './components/Administrators'
@@ -13,6 +13,16 @@ function readCookie() {
   return cookie.load('authorized')
 }
 
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route {...rest} render={props => authed ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/api/discord/login" />
+    )
+  } /> )
+}
+
 function App() {
   const [authed, setAuthed] = useState(readCookie)
 
@@ -22,24 +32,16 @@ function App() {
 
   return (
     <Router>
-    <Route path="/" component={GreenbotNavbar} />
-      {authed ? (
-        <div>
-          <Route path="/point_tables" component={Points} />
-          <Route path="/logs" component={Logs} />
-          <Route path="/administrators" component={Administrators} />
+      <Route path="/" component={GreenbotNavbar} />
+      <PrivateRoute path="/point_tables" component={Points} />
+      <PrivateRoute path="/logs" component={Logs} />
+      <PrivateRoute path="/administrators" component={Administrators} />
+      <Route exact path='/api/discord/login' render={() => (
+        <div style={{display: 'none'}}>
+          {window.location = `http://localhost:5000/api/discord/login`}
         </div>
-      ) : (
-        <div>
-          <Link to="/api/discord/login"><DiscordButton /></Link>
-          <Route exact path='/api/discord/login' render={() => (
-            <div style={{display: 'none'}}>
-              {window.location = `http://localhost:5000/api/discord/login`}
-            </div>
-          )}/>
-          <Route path="/api/discord/confirmed/:token" render={(props) => <TakeParams {...props} setAuthed={setNewAuthed} /> } />
-        </div>
-      )}
+      )}/>
+      <Route path="/api/discord/confirmed/:token" render={(props) => <TakeParams {...props} setAuthed={setNewAuthed} /> } />
     </Router>
   )
 }
