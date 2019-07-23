d148d619cbd72c812e9d4cd55bdcdc941c45156d
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-07-23 13:31:11 +1000

    .env variable for jwt

--- src/TakeParams.js
+++ src/TakeParams.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 import React from 'react';
 import {Redirect } from 'react-router-dom'
 import cookie from 'react-cookie'
@@ -5,7 +6,7 @@ import cookie from 'react-cookie'
 const jwt = require('jsonwebtoken')
 
 function TakeParams(props) {
-  if (jwt.verify(props.match.params.token, 'superSecretKey').authorized == true) {
+  if (jwt.verify(props.match.params.token, process.env.JWT_SECRET_KEY).authorized == true) {
     cookie.save('authorized', true, { maxAge: 60000, path: '/', domain: ".elated-lovelace-d9b735.netlify.com"})
     props.setAuthed(true)
     return (
