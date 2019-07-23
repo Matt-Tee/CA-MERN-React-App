88841259a78e2af9938463c5c1ec9965e0ab75de
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-07-23 14:05:17 +1000

    remove dotenv

--- src/TakeParams.js
+++ src/TakeParams.js
@@ -2,12 +2,10 @@ import React from 'react';
 import {Redirect } from 'react-router-dom'
 import cookie from 'react-cookie'
 
-require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 function TakeParams(props) {
-  console.log(process.env.JWT_SECRET_KEY);
-  if (jwt.verify(props.match.params.token, process.env.JWT_SECRET_KEY).authorized == true) {
+  if (jwt.verify(props.match.params.token, 'superSecretKey').authorized == true) {
     cookie.save('authorized', true, { maxAge: 60000, path: '/', domain: ".elated-lovelace-d9b735.netlify.com"})
     props.setAuthed(true)
     return (
