47f48f9c3bdd6d5bd3da393cc9ee761a9716a764
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-07-23 13:00:27 +1000

    testing if redirect to point tables works

--- src/TakeParams.js
+++ src/TakeParams.js
@@ -9,11 +9,11 @@ function TakeParams(props) {
     cookie.save('authorized', true, { maxAge: 60000, path: '/', domain: ".elated-lovelace-d9b735.netlify.com"})
     props.setAuthed(true)
     return (
-      <Redirect to='/' />
+      <h2>AUthed</h2>
     )
   } else {
     return (
-      <h2>Not AUthed</h2>
+      <Redirect to='/point_tables' />
     )
   }
 }
