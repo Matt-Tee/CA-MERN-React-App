18f03239ceef38613e598458bd7ba7ae8aba189f
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-07-23 13:07:19 +1000

    testing if redirect to point tables works

--- src/TakeParams.js
+++ src/TakeParams.js
@@ -5,17 +5,18 @@ import cookie from 'react-cookie'
 const jwt = require('jsonwebtoken')
 
 function TakeParams(props) {
-  if (jwt.verify(props.match.params.token, 'superSecretKey').authorized == true) {
-    cookie.save('authorized', true, { maxAge: 60000, path: '/', domain: ".elated-lovelace-d9b735.netlify.com"})
-    props.setAuthed(true)
     return (
-      <h2>AUthed</h2>
-    )
-  } else {
-    return (
-      <Redirect to='/point_tables' />
-    )
-  }
+          <Redirect to='/point_tables' />
+        )
+  // if (jwt.verify(props.match.params.token, 'superSecretKey').authorized == true) {
+  //   cookie.save('authorized', true, { maxAge: 60000, path: '/', domain: ".elated-lovelace-d9b735.netlify.com"})
+  //   props.setAuthed(true)
+  //   return (
+  //     <h2>AUthed</h2>
+  //   )
+  // } else {
+  //   )
+  // }
 }
 
 export default TakeParams;
