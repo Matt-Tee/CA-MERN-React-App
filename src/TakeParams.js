feb0d336013b9249de54d5d42cf58602231a76b1
gjohn34 <45950307+gjohn34@users.noreply.github.com>
2019-07-24 14:22:04 +1000

    Update TakeParams.js
    add env var

--- src/TakeParams.js
+++ src/TakeParams.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Redirect } from 'react-router-dom'
 import cookie from 'react-cookie'
+require('dotenv').config()
 
 const jwt = require('jsonwebtoken')
 
 function TakeParams(props) {
   if (jwt.verify(props.match.params.token, 'superSecretKey').authorized == true) {
-    cookie.save('authorized', true, { maxAge: 60000, path: '/', domain: ".serene-crag-19981.herokuapp.com"})
+    cookie.save('authorized', true, { maxAge: 60000, path: '/', domain: process.env.REACT_APP_COOKIE_DESTINATION})
     props.setAuthed(true)
     return (
       <Redirect to='/point_tables' />
